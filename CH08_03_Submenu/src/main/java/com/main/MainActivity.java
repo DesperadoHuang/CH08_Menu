0f782399ec8e12ca2e97ff7d4468aad4900bee36
WilsonHuang <hjn201@gmail.com>
2016-11-02 17:06:39 +0800

    20161102

--- CH08_03_Submenu/src/main/java/com/main/MainActivity.java
+++ CH08_03_Submenu/src/main/java/com/main/MainActivity.java
@@ -1,16 +1,28 @@
 package com.main;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private Context context;
+    private TextView textView_tea, textView_coffee, textView_coolHot;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        context = this;
+
+        textView_tea = (TextView) findViewById(R.id.textView_tea);
+        textView_coffee = (TextView) findViewById(R.id.textView_coffee);
+        textView_coolHot = (TextView) findViewById(R.id.textView_Cool_Hot);
+
+
     }
 
     @Override
