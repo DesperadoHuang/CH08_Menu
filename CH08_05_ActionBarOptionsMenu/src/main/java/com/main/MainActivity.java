4c4861d701e2932a23f8f48ffbd0982b4d0c5adb
WilsonHuang <hjn201@gmail.com>
2016-11-07 17:03:54 +0800

    20161107

--- CH08_05_ActionBarOptionsMenu/src/main/java/com/main/MainActivity.java
+++ CH08_05_ActionBarOptionsMenu/src/main/java/com/main/MainActivity.java
@@ -2,12 +2,41 @@ package com.main;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+    private ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        imageView = (ImageView) findViewById(R.id.imageView);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.actionbar_options_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.game1:
+                imageView.setImageResource(R.drawable.mario);
+                break;
+            case R.id.game2:
+                imageView.setImageResource(R.drawable.sonic);
+                break;
+            case R.id.menu_close:
+                finish();
+                break;
+        }
+        return true;
     }
 }
